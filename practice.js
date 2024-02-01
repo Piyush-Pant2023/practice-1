6965f38522434610063f8d5aac417efc2f76864b
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-02-01 20:17:32 +0530

    basics

---
+++ practice.js
@@ -0,0 +1,9 @@
+function calculateSquareRoots() {
+    for (let i = 1; i <= 10; i++) {
+      const squareRoot = Math.sqrt(i);
+      console.log(`Square root of ${i} is: ${squareRoot}`);
+    }
+  }
+  
+  
+  calculateSquareRoots();
